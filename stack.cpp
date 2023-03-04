af2e8fd7aefae0ef75693470ff29e4ad269810ad
Hernan Cabrera <91387687+GearUltra@users.noreply.github.com>
2023-03-04 15:45:45 -0600

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,1 +1,30 @@
 #include "stack.h"
+#include <stack>
+
+bool validP(string a){
+  stack<char> str;
+  for(int i=0;i<a.length();i++){
+    if(a[i]=='(' || a[i]=='[' || a[i]=='{'){
+      str.push(a[i]);
+    }
+    else if(a[i]==')'){
+      if(str.empty() || str.top() !='('){
+        return false;
+        }
+      str.pop();
+    }
+    else if(a[i]==']'){
+      if(str.empty() || str.top() !='['){
+        return false;
+        }
+      str.pop();
+    }
+    else if(a[i]=='}'){
+      if(str.empty() || str.top() !='{'){
+        return false;
+        }
+      str.pop();
+    }
+  }
+  return str.empty();
+}
